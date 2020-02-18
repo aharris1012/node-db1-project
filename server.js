d26b894f1643753addf79715a774c230b137b9f7
Andrea Harris <Andrea@zacs-imac.lan>
2020-02-18 10:12:59 -0600

    i think in done

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
+const accountsRoutes = require("./routes/accountRoutes");
 
 const server = express();
 
 server.use(express.json());
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "gooodd!" });
+  });
+  server.use("/accounts", accountRoutes);
 
 module.exports = server;
